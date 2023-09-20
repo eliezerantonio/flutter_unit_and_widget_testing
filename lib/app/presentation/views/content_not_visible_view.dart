5302f1b1ae36137973985bff869cfbd124f5b7e9
eliezerantonio <eliezerfernandoantonio@gmail.com>
2023-09-20 20:50:45 +0100

    test(slidable_view): test for type widget slide

--- lib/app/presentation/views/content_not_visible_view.dart
+++ lib/app/presentation/views/content_not_visible_view.dart
@@ -10,29 +10,27 @@ class ContentNotVisibleView extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(),
       body: SafeArea(
-        child: SingleChildScrollView(
-          child: Column(
-            children: [
-              ...Colors.primaries.map(
-                (e) => Container(
-                  height: 50,
-                  color: e,
-                ),
+        child: ListView(
+          children: [
+            ...Colors.primaries.map(
+              (e) => Container(
+                height: 50,
+                color: e,
               ),
-              ElevatedButton(
-                onPressed: () {
-                  showAlertDialog(
-                    context,
-                    message: 'AMAZING CONTENT',
-                  );
-                },
-                child: const Text(
-                  'show message',
-                  style: TextStyle(fontSize: 20),
-                ),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                showAlertDialog(
+                  context,
+                  message: 'AMAZING CONTENT',
+                );
+              },
+              child: const Text(
+                'show message',
+                style: TextStyle(fontSize: 20),
               ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
